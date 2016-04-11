7b0c4eeaa0b3fa56a407944a1f79bd3b0f4cbc1d
Harri <harri@harritaylor.com>
2016-04-11 10:17:24 +0100

    tidied up files a bit

---
+++ main.py
@@ -0,0 +1,47 @@
+import tkinter as tk
+from StartPage import StartPage
+from StudentLogin import StudentLogin
+from MainStudentPage import MainStudentPage
+from ViewLesson1 import ViewLesson1
+from ViewLesson2 import *
+from TakeTest1 import *
+from TakeTest2 import *
+from TeacherLogin import TeacherLogin
+
+
+
+LARGE_FONT= ("Verdana", 12)
+
+
+class main(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+
+        container.pack(side="top", fill="both", expand = True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (StartPage, StudentLogin, TeacherLogin, MainStudentPage, ViewLesson1, ViewLesson2, TakeTest1, TakeTest2):
+
+            frame = F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(column=0, row=0, sticky='nwes')
+
+        self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
+
+# running the app
+app = main()
+app.mainloop()
