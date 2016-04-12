34bd787b814b9cb0ffd196ff3c0d03f8357cfbc2
Harri <harri@harritaylor.com>
2016-04-12 12:37:51 +0100

    questions file

--- StudentLogin.py
+++ StudentLogin.py
@@ -27,28 +27,28 @@ class StudentLogin(tk.Frame):
         log_in_button = tk.Button(self, text="Log in", command=lambda: self.login())
         log_in_button.grid(row=3, column=1)
 
-     
+
         from StartPage import StartPage
 
         button1 = tk.Button(self, text="Back to Home", command=lambda: controller.show_frame(StartPage))
 
         button1.grid(row=3, column=0)
-        
+
     def login(self):
         username = self.e1.get()
-        password = self.e2.get() 
+        password = self.e2.get()
 
 
         my_data = json.loads(open("users.json").read())
         for key, value in my_data.items():
             for i in value:
                 for key1, value1 in i.items():
-                        
-                    if username == key1 and password == value1:            
+
+                    if username == key1 and password == value1:
                         tm.showinfo("Login info", "HI")
                         # self.MainStudentPage()
                         return
 
                     else:
-                        tm.showerror("Login error", "Incorrect username or password")    
+                        tm.showerror("Login error", "Incorrect username or password")
                         return
