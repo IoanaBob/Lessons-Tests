7a9530dc04f2312a856df0d5e60110f936ebf348
Harri <harri@harritaylor.com>
2016-04-12 14:03:46 +0100

    fixed username handling

--- StudentLogin.py
+++ StudentLogin.py
@@ -41,7 +41,12 @@ class StudentLogin(tk.Frame):
         with open('users.json') as data_file:
             user_data = json.load(data_file)
 
-        if user_data['Students'][username] == password:
+        try:
+            username = user_data['Students'][username]
+        except KeyError:
+            tm.showerror("Login Error", "Incorrect Username or pass")
+            return 
+        if username == password:
             controller.show_frame(MainStudentPage)
             return
 
