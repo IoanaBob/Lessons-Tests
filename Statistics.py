e6d4486dac5e91f382d96951859bf23a861cde0c
RedWalters <jonathan.j.walters@Gmail.com>
2016-04-14 20:02:23 +0100

    Update Statistics.py

--- Statistics.py
+++ Statistics.py
@@ -45,51 +45,68 @@ class Statistics(tk.Frame):
             json_decode = json.load(data)
         
         def test1results(controller, jsonfile):
-            test1data = json_decode['username']['test1']
-            test1data = ast.literal_eval(test1data)
-            averagescore= sum(test1data)/len(test1data)
-            topscore = max(test1data)
-            #print("\nScores for Test 1:", test1data)    
-            #print("\nAverage Score for Test 1:",averagescore)
-            #print("\nTop Score for Test 1:",topscore)  
-            amountattempts = list(range(0,len(test1data)))    
-            plt.plot(amountattempts, test1data, 'ro')
-            plt.ylabel("Score")
-            plt.xlabel("Attempts")
-            plt.title('Test 1 Results')
-            plt.axis([-1,len(amountattempts)+1,0,10])
-            plt.show()
+            for i in json_decode:
+                if i == username_stats:                    
+                    test1data = json_decode[i]['1']
+                    #test1data = ast.literal_eval(test1data)
+                    amountofscores = len(test1data)
+                    sumofscores = sum(test1data)
+                    if amountofscores == 0 or sumofscores == 0:
+                        averagescore = "Can't divide by 0!"
+                    else:
+                        averagescore= sum(test1data)/len(test1data)
+                    topscore = max(test1data)
+                    #print("\nScores for Test 1:", test1data)    
+                    #print("\nAverage Score for Test 1:",averagescore)
+                    #print("\nTop Score for Test 1:",topscore)  
+                    amountattempts = list(range(0,len(test1data)))    
+                    plt.plot(amountattempts, test1data, 'ro')
+                    plt.ylabel("Score")
+                    plt.xlabel("Attempts")
+                    plt.title('Test 1 Results')
+                    plt.axis([-1,len(amountattempts)+1,0,10])
+                    plt.show()
             return "\nScores for Test 1:", test1data, "\nAverage Score for Test 1:",averagescore,"\nTop Score for Test 1:",topscore
         
         def test2results(controller, jsonfile):
-            test2data = json_decode['username']['test2']
-            test2data = ast.literal_eval(test2data)
-            averagescore= sum(test2data)/len(test2data)
-            topscore = max(test2data)            
-            #print("\nScores for Test 2:", test2data)    
-            #print("\nAverage Score for Test 2:",averagescore)
-            #print("\nTop Score for Test 2:",topscore)
-            amountattempts = list(range(0,len(test2data)))    
-            plt.plot(amountattempts, test2data, 'ro')
-            plt.ylabel("Score")
-            plt.xlabel("Attempts")
-            plt.title('Test 1 Results')
-            plt.axis([-1,len(amountattempts)+1,0,10])
-            plt.show()
+            for i in json_decode:
+                if i == username_stats:
+                    test2data = json_decode[i]['2']
+                    #test2data = ast.literal_eval(test2data)
+                    amountofscores = len(test2data)
+                    sumofscores = sum(test2data)
+                    if amountofscores == 0 or sumofscores == 0:
+                        averagescore = "Can't divide by 0!"
+                    else:
+                        averagescore= sum(test2data)/len(test2data)
+                        
+                    topscore = max(test2data)            
+                    #print("\nScores for Test 2:", test2data)    
+                    #print("\nAverage Score for Test 2:",averagescore)
+                    #print("\nTop Score for Test 2:",topscore)
+                    amountattempts = list(range(0,len(test2data)))    
+                    plt.plot(amountattempts, test2data, 'ro')
+                    plt.ylabel("Score")
+                    plt.xlabel("Attempts")
+                    plt.title('Test 1 Results')
+                    plt.axis([-1,len(amountattempts)+1,0,10])
+                    plt.show()
             return "\n\nScores for Test 2:", test2data, "\nAverage Score for Test 2:",averagescore,"\nTop Score for Test 2:",topscore
             
         def bothresults(controller, jsonfile):
             test1results(controller, jsonfile)
             test2results(controller, jsonfile)
             
-        for test1 in json_decode:
-            if test1 == username_stats:
-                text = tk.Text(self)
-                text.insert(tk.INSERT, test1results(controller, json_decode))
-                text.insert(tk.INSERT, test2results(controller, json_decode))
-                text.grid(row=5, column=1)
-            else:
-                tm.showerror("Username not found", "Username not found, please try again")
+        #for test1 in json_decode:
+            #if test1 == username_stats:
+        text = tk.Text(self)
+        text.insert(tk.INSERT, test1results(controller, json_decode))
+        text.insert(tk.INSERT, test2results(controller, json_decode))
+        text.grid(row=5, column=1)
+           # else:
+                #tm.showerror("Username not found", "Username not found, please try again")
+        
+        
         
         
         
