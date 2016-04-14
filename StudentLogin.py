8e4ed2fb43b1eaa11ac5b00020f6b2e998af5076
Ivonna <PrinceI1@cardiff.ac.uk>
2016-04-14 22:03:27 +0100

    Update StudentLogin.py

--- StudentLogin.py
+++ StudentLogin.py
@@ -16,10 +16,15 @@ class StudentLogin(tk.Frame):
 
         label = tk.Label(self, text="Log In", font=self.headFont, padx=10, pady=10)
         label.grid(row=0, column=1)
+        label.configure(background = 'white')
 
         # Adding the long in input fields
-        self.number = tk.Label(self, text="Student number:", font=self.titleFont).grid(row=1, sticky="E")
-        self.password = tk.Label(self, text="Password:", font=self.titleFont).grid(row=2, sticky="E")
+        number = tk.Label(self, text="Student number:", font=self.titleFont)
+        number.grid(row=1, sticky="E")
+        number.configure(background = 'white')
+        password = tk.Label(self, text="Password:", font=self.titleFont)
+        password.grid(row=2, sticky="E")
+        password.configure(background = 'white')
 
         self.e1 = tk.Entry(self)
         self.e2 = tk.Entry(self, show="*")
@@ -28,13 +33,14 @@ class StudentLogin(tk.Frame):
         self.e2.grid(row=2, column=1)
         ####################################
         # login button - goes to the lessons
-        log_in_button = tk.Button(self, text="Log in", font=self.buttonFont, padx=4, pady=4, command=lambda: self.login(controller, parent))
+        log_in_button = tk.Button(self, text="Log in", borderwidth = 4, font=self.buttonFont, padx=4, pady=4, command=lambda: self.login(controller, parent))
         log_in_button.grid(row=3, column=1)
+        log_in_button.configure(background = '#FF8800')
 
 
         from StartPage import StartPage
 
-        button1 = tk.Button(self, text="Back to Home", font=self.buttonFont, padx=4, pady=4, command=lambda: controller.show_frame(StartPage))
+        button1 = tk.Button(self, text="Back to Home",relief = tk.RIDGE, borderwidth = 4, font=self.buttonFont, padx=4, pady=4, command=lambda: controller.show_frame(StartPage))
 
         button1.grid(row=0, column=0)
 
