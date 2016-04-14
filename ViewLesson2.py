1ca51846ed9919054b9616968ab7c0f7b3a5048f
Jake <jakeelliotmorgan@gmail.com>
2016-04-14 12:11:41 +0100

    more ux

--- ViewLesson2.py
+++ ViewLesson2.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import json
 from TakeTest2 import TakeTest2
+from tkinter import font
 LARGE_FONT= ("Verdana", 12)
 
 with open('lessons.json') as data_file:
@@ -23,33 +24,37 @@ class ViewLesson2(tk.Frame):
         #=====================================
         # MENU STARTS HERE
         # TODO: make buttons stay one near each other (not depending on the other columns)
-        menu1 = tk.Button(self, text="Lesson 1", command=lambda: controller.show_frame(ViewLesson1))
-        menu1.grid(row=0, column=0)
-        menu2 = tk.Button(self, text="Lesson 2", command=lambda: controller.show_frame(ViewLesson2))
-        menu2.grid(row=0, column=1)
-        menu3 = tk.Button(self, text="Test 1", command=lambda: controller.show_frame(TakeTest1))
-        menu3.grid(row=0, column=2)
-        menu4 = tk.Button(self, text="Test 2", command=lambda: controller.show_frame(TakeTest2))
-        menu4.grid(row=0, column=3)
-        menu5 = tk.Button(self, text="My Grades", command=lambda: controller.show_frame(MyGrades))
-        menu5.grid(row=0, column=4)
-        menu6 = tk.Button(self, text="Log Out", command=lambda: controller.show_frame(StartPage))
-        menu6.grid(row=0, column=5)
+
+        self.headFont = font.Font(family="Helvetica Neue Light", weight="normal", size=30)
+        self.titleFont = font.Font(family="Helvetica Neue Light", weight="normal", size=20)
+        self.buttonFont = font.Font(family="Helvetica Neue Light", weight="normal", size=18)
+        self.textFont = font.Font(family="Helvetica Neue Light", weight="normal", size=12)
+
+        menu0 = tk.Button(self, text="Home", padx=4, pady=4, font=self.buttonFont, command=lambda: controller.show_frame(MainStudentPage))
+        menu0.grid(row=0, column=0)
+        menu1 = tk.Button(self, text="Probability Lesson", padx=4, pady=4, font=self.buttonFont, command=lambda: controller.show_frame(ViewLesson1))
+        menu1.grid(row=0, column=1)
+        menu2 = tk.Button(self, text="Sets Lesson", padx=4, pady=4, font=self.buttonFont, command=lambda: controller.show_frame(ViewLesson2))
+        menu2.grid(row=0, column=2)
+        menu3 = tk.Button(self, text="Probability Test", padx=4, pady=4, font=self.buttonFont, command=lambda: controller.show_frame(TakeTest1))
+        menu3.grid(row=0, column=3)
+        menu4 = tk.Button(self, text="Sets Test", padx=4, pady=4, font=self.buttonFont, command=lambda: controller.show_frame(TakeTest2))
+        menu4.grid(row=0, column=4)
+        menu5 = tk.Button(self, text="My Grades", padx=4, pady=4, font=self.buttonFont, command=lambda: controller.show_frame(MyGrades))
+        menu5.grid(row=0, column=5)
+        menu6 = tk.Button(self, text="Log Out", padx=4, pady=4, font=self.buttonFont, command=lambda: controller.show_frame(StartPage))
+        menu6.grid(row=0, column=6)
         #=====================================
 
-        label = tk.Label(self, text=lesson_data['Lessons'][1]['Lesson Title'], font=LARGE_FONT)
-        label.grid(row=1)
+        label = tk.Label(self, text=lesson_data['Lessons'][1]['Lesson Title'], font=self.headFont)
+        label.grid(row=1, columnspan=2)
 
-        text = tk.Text(self)
+        text = tk.Text(self, font=self.textFont)
         text.insert(tk.INSERT, lesson_data['Lessons'][1]['Lesson Content'])
 
         text.config(state=tk.DISABLED)
-        text.grid(row=2)
+        text.grid(row=2, columnspan=7)
 
         # Go to take test
-        button1 = tk.Button(self, text="Take test", command=lambda: controller.show_frame(TakeTest2))
-        button1.grid(row=3)
-
-        # back to lessons
-        button2 = tk.Button(self, text="Back to Lessons", command=lambda: controller.show_frame(MainStudentPage))
-        button2.grid(row=4)
+        button1 = tk.Button(self, text="Continue and take test on sets", font=self.buttonFont, padx=4, pady=4, command=lambda: controller.show_frame(TakeTest2))
+        button1.grid(row=3, columnspan=7)
