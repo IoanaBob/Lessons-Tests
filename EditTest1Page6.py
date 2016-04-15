7ff6dd29d84b64be1d3c2a78c48917058f139d33
Jake <jakeelliotmorgan@gmail.com>
2016-04-15 01:51:11 +0100

    changed menu bar

--- EditTest1Page6.py
+++ EditTest1Page6.py
@@ -27,21 +27,36 @@ class EditTest1Page6(tk.Frame):
         from EditTest1Page4 import EditTest1Page4
         from EditTest1Page5 import EditTest1Page5
         from EditTest1Page6 import EditTest1Page6
+        from MainTeacherPage import MainTeacherPage
         #=====================================
+
+        self.headFont = font.Font(family="Helvetica Neue Light", weight="normal", size=30)
+        self.titleFont = font.Font(family="Helvetica Neue Light", weight="normal", size=20)
+        self.buttonFont = font.Font(family="Helvetica Neue Light", weight="normal", size=18)
+
         # MENU STARTS HERE
         # TODO: make buttons stay one near each other (not depending on the other columns)
-        menu1 = tk.Button(self, text="Edit Lesson 1", command=lambda: controller.show_frame(EditLesson1))
+        menu1 = tk.Button(self, text="Edit Probability Lesson",  padx=4, pady=4, font=self.buttonFont, command=lambda: controller.show_frame(EditLesson1))
         menu1.grid(row=0, column=0)
-        menu2 = tk.Button(self, text="Edit Lesson 2", command=lambda: controller.show_frame(EditLesson2))
+        menu1.configure(background = '#FF8800')
+        menu2 = tk.Button(self, text="Edit Sets Lesson",  padx=4, pady=4, font=self.buttonFont, command=lambda: controller.show_frame(EditLesson2))
         menu2.grid(row=0, column=1)
-        menu3 = tk.Button(self, text="Modify Test 1", command=lambda: controller.show_frame(EditTest1))
+        menu2.configure(background = '#FF8800')
+        menu3 = tk.Button(self, text="Modify Probability Test",  padx=4, pady=4, font=self.buttonFont, command=lambda: controller.show_frame(EditTest1))
         menu3.grid(row=0, column=2)
-        menu4 = tk.Button(self, text="Modify Test 2", command=lambda: controller.show_frame(EditTest2))
+        menu3.configure(background = '#FF8800')
+        menu4 = tk.Button(self, text="Modify Sets Test",  padx=4, pady=4, font=self.buttonFont, command=lambda: controller.show_frame(EditTest2))
         menu4.grid(row=0, column=3)
-        menu5 = tk.Button(self, text="Statistics", command=lambda: controller.show_frame(Statistics))
+        menu4.configure(background = '#FF8800')
+        menu5 = tk.Button(self, text="Statistics",  padx=4, pady=4, font=self.buttonFont, command=lambda: controller.show_frame(Statistics))
         menu5.grid(row=0, column=4)
-        menu6 = tk.Button(self, text="Log Out", command=lambda: controller.show_frame(StartPage))
-        menu6.grid(row=0, column=5)
+        menu5.configure(background = '#FF8800')
+        menu6 = tk.Button(self, text="Log Out",  padx=4, pady=4, font=self.buttonFont, command=lambda: controller.show_frame(StartPage))
+        menu6.grid(row=0, column=6)
+        menu6.configure(background = '#FF8800')
+        menu7 = tk.Button(self, text="Back Home",  padx=4, pady=4, font=self.buttonFont, command=lambda: controller.show_frame(MainTeacherPage))
+        menu7.grid(row=0, column=5)
+        menu7.configure(background = '#FF8800')
         #=====================================
         # 2nd menu
         q0 = tk.Button(self, text="Question 1", command=lambda: controller.show_frame(EditTest1Page0))
